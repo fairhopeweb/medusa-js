72a5700a20187d009881f52b6e14e7f1f85cdbea
Vilfred Sikker <vilfredsikker@gmail.com>
2020-12-21 10:28:38 +0100

    added swap

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,35 @@
 import Client from './request';
 import { Config } from './request';
 import CartsResource from './resources/carts';
+import CustomerResource from './resources/customer';
 import OrdersResource from './resources/orders';
 import ProductsResource from './resources/products';
 import RegionsResource from './resources/regions';
 import ShippingOptionsResource from './resources/shipping-options';
+import SwapResource from './resources/swap';
 
 class Medusa {
   private client: Client;
-  public products: ProductsResource;
   public carts: CartsResource;
-  public orders: OrdersResource;
-  public shippingOptions: ShippingOptionsResource;
-  public regions: RegionsResource;
+  public customers: CustomerResource;
   public errors: MedusaError;
+  public orders: OrdersResource;
+  public products: ProductsResource;
+  public regions: RegionsResource;
+  public shippingOptions: ShippingOptionsResource;
+  public swap: SwapResource;
 
   constructor(config: Config) {
     this.client = new Client(config);
 
-    this.products = new ProductsResource(this.client);
     this.carts = new CartsResource(this.client);
-    this.orders = new OrdersResource(this.client);
-    this.shippingOptions = new ShippingOptionsResource(this.client);
-    this.regions = new RegionsResource(this.client);
+    this.customers = new CustomerResource(this.client);
     this.errors = new MedusaError();
+    this.orders = new OrdersResource(this.client);
+    this.products = new ProductsResource(this.client);
+    this.regions = new RegionsResource(this.client);
+    this.shippingOptions = new ShippingOptionsResource(this.client);
+    this.swap = new SwapResource(this.client);
   }
 }
 
